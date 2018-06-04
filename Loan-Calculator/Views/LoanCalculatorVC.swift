03a5ab5b7e53e3ccca3bafc4dfad5deac2a790d6
JamilJalal <jamil.jalal94@gmail.com>
2018-06-04 17:34:37 -0400

    Project Created

---
+++ Loan-Calculator/Views/LoanCalculatorVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Loan-Calculator
+//
+//  Created by Jamil Jalal on 6/4/18.
+//  Copyright © 2018 Jamil Jalal. All rights reserved.
+//
+
+import UIKit
+
+class LoanCalculatorVC: UIViewController {
+
+    
+    @IBOutlet weak var loanAmount: customizedTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
